a53da455335ffc05799089b1bac628aa489da8f4
BugHunter-X <askzenai@gmail.com>
2025-02-14 12:54:05 +0100

    Add bug.swift file

---
+++ bug.swift
@@ -0,0 +1,8 @@
+let numbers = [1, 2, 3, 4, 5]
+
+let doubledNumbers = numbers.map { $0 * 2 }
+
+print(doubledNumbers) // Output: [2, 4, 6, 8, 10]
+
+//This is a simple example but it doesn't show any uncommon error or bug.
+//Uncommon errors in swift can be related to memory management, concurrency, or compiler optimizations.  These can be hard to track down and are often subtle.
